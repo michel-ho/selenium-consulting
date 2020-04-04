60967173713b2160d63cc2443c429c7b3e7b4187
floruo <florian.ruosch@uzh.ch>
2020-04-04 21:37:03 +0200

    toward actual stats + stats in table

--- src/app/statistic/statistic.component.ts
+++ src/app/statistic/statistic.component.ts
@@ -2,8 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Statistik} from '../../../api/statistik';
+import {Kanton} from '../../../api/kanton';
+import {KurzArbeitVoranmeldung} from '../../../api/kurzArbeitVoranmeldung';
 import {Api} from '../../../api/api';
 import {Router} from '@angular/router';
+import data from '../../../api/db.json';
 
 @Component({
   selector: 'app-statistic',
@@ -15,10 +18,25 @@ export class StatisticComponent implements OnInit {
   statistik$: Observable<Statistik> = this.http.get<Statistik>(Api.STATISTIK);
   selectedStatistik: Statistik = null;
 
+  public statistikData:Statistik[] = data['statistik'];
+
+  public kantone:Kanton[] = data['kanton'];
+  public kAV:KurzArbeitVoranmeldung[] = data['kurzArbeitVoranmeldung'];
+  public stats:Statistik[];
+
+
   constructor(private http: HttpClient,
               private router: Router) {}
 
   ngOnInit() {
+    for (let k of this.kantone) {
+      this.stats = [ {} ]
+    }
+    for (let k of this.kAV) {
+      //console.log(k.arbeitgeber);
+    }
+    /*for (let t of this.stats) {
+      console.log(t.id);
+    }*/
   }
-
 }
